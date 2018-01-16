2634d57cb3759144d41267d125ebc4d1abe43baf
Brian Beagle <phbeagle@gmail.com>
2018-01-16 12:53:50 -0600

    Add project files.

---
+++ ConsoleAppForCSharpSandbox/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForCSharpSandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Task(() => {
+                Console.WriteLine("task 1 is beginning");
+                Thread.Sleep(1000);
+                Console.WriteLine("task 1 has completed");
+            });
+            t1.Start();
+
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
